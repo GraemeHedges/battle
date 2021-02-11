cb64fe84ff009b3f46ecd35a202f6f57ea114e77
Graemehedges <graeme.hedges@gmail.com>
2021-02-11 11:48:09 +0000

    made changes in line with walkthrough - fleshed out the player class more with additional methods, default HP and tidied up the controller gets

--- spec/features/player_attack_spec.rb
+++ spec/features/player_attack_spec.rb
@@ -1,8 +1,14 @@
-feature 'Testing player 1 can attack player 2' do
-  scenario 'Player 1 attacks players 2 reduing their HP by 10' do
+feature 'Testing Player 1 can attack Player 2' do
+  scenario 'Player 1 attacks Player 2 ' do
     sign_in_and_play
     click_button 'Attack'
     expect(page).to have_content 'Player 1 attacked Player 2, it is super effective!'
+  end
+
+  scenario 'reduce Player 2 HP by 10' do
+    sign_in_and_play
+    click_button 'Attack'
+    expect(page).not_to have_content 'Player 2: 60HP'
     expect(page).to have_content 'Player 2: 50HP'
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,31 @@
 require 'player'
 
 describe Player do
-  subject(:player) {Player.new('Oliver')}
+  subject(:player_1) {Player.new('Player 1')}
+  subject(:player_2) {Player.new('Player 2')}
 
   describe '#name' do
     it 'returns the players name' do
-      expect(subject.name).to eq 'Oliver'
+      expect(player_1.name).to eq 'Player 1'
+    end
+  end
+
+  describe '#hit_points' do
+    it 'returns the hit points' do
+      expect(player_1.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(player_2).to receive(:receive_damage)
+      player_1.attack(player_2)
+    end
+  end
+
+  describe '#recieve_damage' do
+    it 'reduces the players hit points' do
+      expect { player_1.receive_damage}.to change { player_1.hit_points}.by(-10)
     end
   end
 end
