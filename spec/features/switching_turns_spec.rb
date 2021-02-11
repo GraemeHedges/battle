ad4e4f0d30349eea5dacce9c5e931d83c60fc636
Graemehedges <graeme.hedges@gmail.com>
2021-02-11 13:20:48 +0000

    we full walkthrough now

---
+++ spec/features/switching_turns_spec.rb
@@ -0,0 +1,15 @@
+feature 'Testing ability to switch turns' do
+  context ' seeing current turn' do
+    scenario 'at the start of the game' do
+      sign_in_and_play
+      expect(page).to have_content "Player 1's turn"
+    end
+
+    scenario 'after Player 1 attacks' do
+      sign_in_and_play
+      click_button 'Attack'
+      expect(page).not_to have_content "Player 1's turn"
+      expect(page).to have_content "Player 2's turn"
+    end
+  end
+end
